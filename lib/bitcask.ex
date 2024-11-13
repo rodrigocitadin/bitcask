c702663e50458ea9ffea875de4b30c5d2b6f4267
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-13 11:13:39 -0300

    start

---
+++ lib/bitcask.ex
@@ -0,0 +1,18 @@
+defmodule Bitcask do
+  @moduledoc """
+  Documentation for `Bitcask`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bitcask.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bitcask_test.exs
@@ -0,0 +1,8 @@
+defmodule BitcaskTest do
+  use ExUnit.Case
+  doctest Bitcask
+
+  test "greets the world" do
+    assert Bitcask.hello() == :world
+  end
+end
