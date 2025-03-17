7e9140d2aaa745169dfb2d1c957cd98ee0a2745e
rodrigocitadin <citadinrodrigo@gmail.com>
2025-03-17 13:13:02 -0300

    feat: get value by key

--- bitcask/operations.go
+++ bitcask/operations.go
@@ -2,6 +2,7 @@ package bitcask
 
 import (
 	"encoding/binary"
+	"errors"
 	"io"
 )
 
@@ -30,4 +31,26 @@ func (bc *Bitcask) Put(key string, value []byte) error {
 
 	bc.index[key] = Entry{Offset: offset, Length: len(buf)}
 	return nil
+}
+
+func (bc *Bitcask) Get(key string) ([]byte, error) {
+	bc.mu.RLock()
+	defer bc.mu.RUnlock()
+
+	entry, exists := bc.index[key]
+	if !exists {
+		return nil, errors.New("Key not found")
+	}
+
+	buf := make([]byte, entry.Length)
+	_, err := bc.file.ReadAt(buf, entry.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	keySize := binary.LittleEndian.Uint64(buf[:8])
+	valueSize := binary.LittleEndian.Uint64(buf[8+keySize : 16+keySize])
+	value := buf[16+keySize : 16+keySize+valueSize]
+
+	return value, nil
 }
