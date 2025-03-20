cd599443a2dd3b531b233b04fc1ac7fd94ea2faa
rodrigocitadin <citadinrodrigo@gmail.com>
2025-03-20 11:08:59 -0300

    starting from scratch again

--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"rodrigocitadin/bitcask/bitcask"
-)
-
 func main() {
-	db, err := bitcask.Open("bitcask.db")
-	if err != nil {
-		log.Fatalf("Error opening database: %v", err)
-	}
-	defer db.Close()
 
-	fmt.Println("Inserting values...")
-	db.Put("user:1", []byte("Alice"))
-	db.Put("user:2", []byte("Bob"))
-	db.Put("user:3", []byte("Charlie"))
-
-	fmt.Println("Available keys:", db.ListKeys())
-
-	value, err := db.Get("user:2")
-	if err != nil {
-		log.Fatalf("Error getting user:2: %v", err)
-	}
-        fmt.Printf("user:2 value -> %s\n", value)
-
-	fmt.Println("Removing user:1...")
-	db.Delete("user:1")
-
-	fmt.Println("Keys aftermath:", db.ListKeys())
 }
